3a4c42845f950c43fe58bf3ebea83318e9680d35
wmcs520 <115633523+wmcs520@users.noreply.github.com>
2024-05-13 13:53:33 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html><html><head><meta charset="utf-8">
+<title>初始化</title><meta name="viewport" content="width=device-width,height=device-height,user-scalable=no,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0"><style>
+*{padding:0;margin:0;}body{font-family:serif;background-color:#eee;}
+</style></head><body><div class="box">
+<?php echo("测试成功"); ?>
+</div></body>
+<script type="text/javascript">
+</script>
+</html>
