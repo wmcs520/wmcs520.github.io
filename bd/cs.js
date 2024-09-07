4f67a701723fd618801d275e6684d2d69c7e3acb
wmcs520 <115633523+wmcs520@users.noreply.github.com>
2024-09-07 16:35:47 +0800

    Add files via upload

---
+++ bd/cs.js
@@ -0,0 +1,2 @@
+function bj_cs(){document.body.style.backgroundColor="#000000";}
+function bj_cs2(){document.body.style.backgroundImage="url('http://gips3.baidu.com/it/u=3886271102,3123389489&fm=3028&app=3028&f=JPEG&fmt=auto?w=1280&h=960')";}
